3f642408ae1d9bff1fe5cde2bd0f69a2c55a6dde
Xwilarg <xwilarg@protonmail.com>
2026-04-11 17:21:34 +0200

    Implement butterfly radar

--- Assets/Scripts/Player/Net.cs
+++ Assets/Scripts/Player/Net.cs
@@ -1,13 +1,18 @@
 using NGJ2026.Insect;
 using NGJ2026.Manager;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace NGJ2026.Player
 {
     public class Net : MonoBehaviour
     {
+        private Vector3 _lastRefPoint;
         [SerializeField]
         private Transform _velRefPoint;
+
+        private readonly List<FloatDT> _velocities = new();
 
         private void OnTriggerEnter(Collider other)
         {
@@ -16,5 +21,28 @@ namespace NGJ2026.Player
                 InsectManager.Instance.CatchButterfly(b);
             }
         }
+
+        private void Awake()
+        {
+            _lastRefPoint = _velRefPoint.position;
+        }
+
+        private void Update()
+        {
+            _velocities.Add(new() { Time = Time.unscaledTime, Value = (_velRefPoint.position - _lastRefPoint).magnitude });
+
+            _velocities.RemoveAll(x => Time.unscaledTime - x.Time > 1f);
+        }
+
+        public float GetSumVelocity()
+        {
+            return _velocities.Sum(x => x.Value);
+        }
+    }
+
+    public record FloatDT
+    {
+        public float Time;
+        public float Value;
     }
 }
